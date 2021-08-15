63352c9701bb386288075d6f861ccde49a7b4fe1
hgrranzi <vkh.mariia@gmail.com>
2021-08-15 10:51:15 +0300

    Renamed the project

--- 1 - AmazingNumbers/AmazingNumbers.java
+++ 1 - AmazingNumbers/AmazingNumbers.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class AmazingNumbers {
     public static void main(String[] args) {
         Scanner term = new Scanner(System.in);
         String input;
