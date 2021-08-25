6bc8b9cddd60365d309acce5b68af0fcca7ff009
hgrranzi <vkh.mariia@gmail.com>
2021-08-25 12:03:59 +0300

    Created the calculateTotalIncome method

--- 2 - CinemaRoomManager/Cinema.java
+++ 2 - CinemaRoomManager/Cinema.java
@@ -3,9 +3,40 @@ import java.util.*;
 public class Cinema {
 
     public static void main(String[] args) {
-        char[][] scheme = createScheme(7, 8);
+        Scanner term = new Scanner(System.in);
+        char[][] scheme;
+        int rows;
+        int seatsInRow;
+        int totalIncome;
+        int priceFrontSeat = 10;
+        int priceBackSeat = 8;
 
-        printScheme(scheme);
+        System.out.println("Enter the number of rows:");
+        rows = term.nextInt();
+        System.out.println("Enter the number of seats in each row:");
+        seatsInRow = term.nextInt();
+        totalIncome = calculateTotalIncome(rows, seatsInRow, priceFrontSeat, priceBackSeat);
+        printTotalIncome(totalIncome);
+
+        //scheme = createScheme(rows, seatsInRow);
+        //printScheme(scheme);
+    }
+
+    public static int calculateTotalIncome(int rows, int seats, int priceFront, int priceBack) {
+        int frontSeats;
+        int backSeats;
+
+        if (rows * seats <= 60) {
+            return rows * seats * priceFront;
+        }
+        frontSeats = rows / 2 * seats;
+        backSeats = rows * seats - frontSeats;
+        return (frontSeats * priceFront + backSeats * priceBack);
+    }
+
+    public static void printTotalIncome(int total) {
+        System.out.println("Total income:");
+        System.out.printf("$%d", total);
     }
 
     public static char[][] createScheme(int rows, int seats) {
