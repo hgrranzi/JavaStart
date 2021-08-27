156cc1b3c90a44fccc95c7b32362e712e2f41616
hgrranzi <vkh.mariia@gmail.com>
2021-08-27 09:22:57 +0300

    Created the calculating ingrediends method

--- 3 - CoffeeMachine/CoffeeMachine.java
+++ 3 - CoffeeMachine/CoffeeMachine.java
@@ -2,6 +2,22 @@ import java.util.*;
 
 public class CoffeeMachine {
     public static void main(String[] args) {
+        Scanner term = new Scanner(System.in);
+        int cups;
+
+        System.out.println("Write how many cups of coffee you will need:");
+        cups = term.nextInt();
+        calculateAndPrintIngredients(cups);
+    }
+
+    public static void calculateAndPrintIngredients(int cups) {
+        System.out.printf("For %d cups of coffee you will need:\n", cups);
+        System.out.printf("%d ml of water\n", 200 * cups);
+        System.out.printf("%d ml of milk\n", 50 * cups);
+        System.out.printf("%d g of coffee beans\n", 15 * cups);
+    }
+
+    public static void printProcess() {
         System.out.println("Starting to make a coffee\n" +
                 "Grinding coffee beans\n" +
                 "Boiling water\n" +
