028e66a047d0b8f1ce804ad8ef0d01c28e07e4f2
hgrranzi <vkh.mariia@gmail.com>
2021-09-09 12:29:52 +0300

    Added a decription function

--- 5 - EncryptionDecryption/encryptdecrypt/EncryptDecrypt.java
+++ 5 - EncryptionDecryption/encryptdecrypt/EncryptDecrypt.java
@@ -4,10 +4,15 @@ import java.util.*;
 public class EncryptDecrypt {
     public static void main(String[] args) {
         Scanner term = new Scanner(System.in);
+        String option = term.nextLine();
         String message = term.nextLine();
         int key = term.nextInt();
 
-        System.out.println(encryptShift(message, key));
+        if ("enc".equals(option)) {
+            System.out.println(encryptShift(message, key));
+        } else if ("dec".equals(option)) {
+            System.out.println(decryptShift(message, key));
+        }
     }
 
     static char[] encryptShift(String text, int key) {
@@ -15,19 +20,24 @@ public class EncryptDecrypt {
 
         encryptedText = new char[text.length()];
         for (int i = 0; i < encryptedText.length; i++) {
-            encryptedText[i] = encryptChar(text.charAt(i), key);
+            encryptedText[i] = (char) (text.charAt(i) + key);
+            //if (encryptedText[i] > 122) {
+            //    encryptedText[i] = (char) (96 + (encryptedText[i] - 122));
+            //}
         }
-        return  encryptedText;
+        return encryptedText;
     }
 
-    static char encryptChar(char c, int key) {
-        char res = c;
-        if (Character.isAlphabetic(res)) {
-            res += key;
-            if (res > 122) {
-                res = (char) (96 + (res - 122));
-            }
+    static char[] decryptShift(String text, int key) {
+        char[] encryptedText;
+
+        encryptedText = new char[text.length()];
+        for (int i = 0; i < encryptedText.length; i++) {
+            encryptedText[i] = (char) (text.charAt(i) - key);
+            //if (encryptedText[i] < 97) {
+             //   encryptedText[i] = (char) (123 - (97 - encryptedText[i]));
+            //}
         }
-        return res;
+        return encryptedText;
     }
 }
