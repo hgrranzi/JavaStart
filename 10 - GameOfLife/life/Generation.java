3cef75c4d3968e0b09655de1036ba2c75f5336bc
hgrranzi <vkh.mariia@gmail.com>
2021-11-02 16:17:38 +0300

    Counted live around

--- 10 - GameOfLife/life/Generation.java
+++ 10 - GameOfLife/life/Generation.java
@@ -50,7 +50,50 @@ public class Generation {
 
     static int countLiveAround(char[][] currentField, int i, int j) {
         int count = 0;
+        boolean ySmall = false;
+        boolean xSmall = false;
+        boolean yBig = false;
+        boolean xBig = false;
 
+        for (int y = i - 1; y <= i + 1; y++) {
+            if (y == -1) {
+                y += currentField.length;
+                ySmall = true;
+            }
+            if (y == currentField.length) {
+                y = 0;
+                yBig = true;
+            }
+            for (int x = j - 1; x <= j + 1; x++) {
+                if (x == -1) {
+                    x += currentField.length;
+                    xSmall = true;
+                }
+                if (x == currentField.length) {
+                    x = 0;
+                    xBig = true;
+                }
+                if (currentField[y][x] == 'O') {
+                    count++;
+                }
+                if (xSmall) {
+                    xSmall = false;
+                    x = -1;
+                }
+                if (xBig) {
+                    xBig = false;
+                    x = currentField.length;
+                }
+            }
+            if (ySmall) {
+                ySmall = false;
+                y = -1;
+            }
+            if (yBig) {
+                yBig = false;
+                y = currentField.length;
+            }
+        }
         return count;
     }
 }
